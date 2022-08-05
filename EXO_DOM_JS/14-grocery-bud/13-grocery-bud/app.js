8069b421eea0232847d28ed8e216a4dd8880f636
Ultrasour8 <tristan.mattei@gmail.com>
2022-08-05 11:46:39 +0200

    +TODO_LIST_JS

--- EXO_DOM_JS/14-grocery-bud/13-grocery-bud/app.js
+++ EXO_DOM_JS/14-grocery-bud/13-grocery-bud/app.js
@@ -3,6 +3,11 @@ const clear = document.querySelector('.clear-btn');
 const submit = document.querySelector('.submit-btn');
 const form = document.querySelector('.grocery-form');
 const grocery = document.getElementById('grocery');
+const alert = document.querySelector('.alert');
+const list = document.querySelector('.grocery-list');
+const container = document.querySelector('.grocery-container');
+const editBtn = document.querySelector('.edit-btn');
+const delBtn = document.querySelector('.delete-btn');
 let task = [];
 // variables pour l'option éditer
 let editElement;
@@ -21,35 +26,89 @@ function addItem(e) {
   e.preventDefault();
   value = grocery.value;
   const id = new Date().getTime().toString();
-  console.log(grocery.value);
-  console.log(id);
-  if (value && editFlag === false) {
-  } else if (value && editFlag === true) {
+  if (value && !editFlag) {
+    const element = document.createElement('article');
+    element.classList.add('grocery-items');
+    const dataId = document.createAttribute('data-id');
+    dataId.value = id;
+    element.innerHTML = `
+    <p class="title">${value}</p>
+      <div class="btn-container">
+        <button type="button" class="edit-btn">
+          <i class="fa-solid fa-pen-to-square"></i>
+          </button>
+          <button type="button" class="delete-btn">
+          <i class="fa-solid fa-trash-can"></i>
+          </button>
+      </div>
+    `;
+    list.appendChild(element);
+    displayAlert('bravo une nouvelle chose à acheter', 'success');
+    container.classList.add('show-container');
+    setBackToDefault();
+  } else if (value && editFlag) {
   } else {
+    displayAlert('il faut entrer quelque chose', 'danger');
   }
 }
 // display alert
-function displayAlert(text, action) {}
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(function () {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1500);
+}
 
-function setBackToDefault() {}
+function setBackToDefault() {
+  if (value) {
+    value == '';
+  }
+  if (editFlag) {
+    editFlag === false;
+  }
+  if (editID) {
+    editID == '';
+  }
+}
 
 // clear items
-function clearItems() {}
+clear.addEventListener('click', clearItems);
+
+function clearItems() {
+  const elements = document.querySelectorAll('article');
+  elements.forEach(function (element) {
+    element.remove();
+  });
+  displayAlert('vous avez supprimer toute les courses', 'danger');
+}
 
 // delete item
-function deleteItem(e) {}
+delBtn.addEventListener('click', deleteItem);
+function deleteItem(e) {
+  console.log(e);
+  e.target.remove();
+  displayAlert('vous avez bien supprimer la tâche', 'danger');
+}
 
 // edit item
+editBtn.addEventListener('click', editItem);
 function editItem(e) {}
 
 // ****** LOCAL STORAGE **********
-function addToLocalStorage(id, value) {}
+function addToLocalStorage(id, value) {
+  localStorage.setItem(id, value);
+}
 
 function editLocalStorage(id, value) {}
 
 function getLocalStorage() {}
 
 // ****** SETUP ITEMS **********
+
+// recupere le tableau du local storage
+// domcontentloaded if ! create []
 function setupItems() {}
 
 function createListItem(id, value) {}
